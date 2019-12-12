c4d2f8da8d3c70f980e3b0ec2c675a4a9315a953
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-12 13:33:57 +0100

    Fixed the importations

--- route_thread.go
+++ route_thread.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/sausheong/gwp/Chapter_2_Go_ChitChat/chitchat/data"
 	"net/http"
+
+	"github.com/Oyekunle-Mark/wailing-network/data"
 )
 
 // GET /threads/new
