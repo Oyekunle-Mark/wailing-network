1a74f0f9db444b1be8b178dc651688e70bd32c55
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-12 14:21:31 +0100

    Auth helpers

--- route_auth.go
+++ route_auth.go
@@ -23,17 +23,21 @@ func signup(writer http.ResponseWriter, request *http.Request) {
 // Create the user account
 func signupAccount(writer http.ResponseWriter, request *http.Request) {
 	err := request.ParseForm()
+
 	if err != nil {
 		danger(err, "Cannot parse form")
 	}
+
 	user := data.User{
 		Name:     request.PostFormValue("name"),
 		Email:    request.PostFormValue("email"),
 		Password: request.PostFormValue("password"),
 	}
+
 	if err := user.Create(); err != nil {
 		danger(err, "Cannot create user")
 	}
+
 	http.Redirect(writer, request, "/login", 302)
 }
 
@@ -42,19 +46,24 @@ func signupAccount(writer http.ResponseWriter, request *http.Request) {
 func authenticate(writer http.ResponseWriter, request *http.Request) {
 	err := request.ParseForm()
 	user, err := data.UserByEmail(request.PostFormValue("email"))
+
 	if err != nil {
 		danger(err, "Cannot find user")
 	}
+
 	if user.Password == data.Encrypt(request.PostFormValue("password")) {
 		session, err := user.CreateSession()
+
 		if err != nil {
 			danger(err, "Cannot create session")
 		}
+
 		cookie := http.Cookie{
 			Name:     "_cookie",
 			Value:    session.Uuid,
 			HttpOnly: true,
 		}
+
 		http.SetCookie(writer, &cookie)
 		http.Redirect(writer, request, "/", 302)
 	} else {
@@ -67,10 +76,12 @@ func authenticate(writer http.ResponseWriter, request *http.Request) {
 // Logs the user out
 func logout(writer http.ResponseWriter, request *http.Request) {
 	cookie, err := request.Cookie("_cookie")
+
 	if err != http.ErrNoCookie {
 		warning(err, "Failed to get cookie")
 		session := data.Session{Uuid: cookie.Value}
 		session.DeleteByUUID()
 	}
+
 	http.Redirect(writer, request, "/", 302)
 }
