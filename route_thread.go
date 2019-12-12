c7c98f304b6742b466ab6599546663283a09cccb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-12 14:23:43 +0100

    Thread helper

--- route_thread.go
+++ route_thread.go
@@ -11,6 +11,7 @@ import (
 // Show the new thread form page
 func newThread(writer http.ResponseWriter, request *http.Request) {
 	_, err := session(writer, request)
+
 	if err != nil {
 		http.Redirect(writer, request, "/login", 302)
 	} else {
@@ -29,14 +30,19 @@ func createThread(writer http.ResponseWriter, request *http.Request) {
 		if err != nil {
 			danger(err, "Cannot parse form")
 		}
+
 		user, err := sess.User()
+
 		if err != nil {
 			danger(err, "Cannot get user from session")
 		}
+
 		topic := request.PostFormValue("topic")
+
 		if _, err := user.CreateThread(topic); err != nil {
 			danger(err, "Cannot create thread")
 		}
+
 		http.Redirect(writer, request, "/", 302)
 	}
 }
@@ -47,10 +53,12 @@ func readThread(writer http.ResponseWriter, request *http.Request) {
 	vals := request.URL.Query()
 	uuid := vals.Get("id")
 	thread, err := data.ThreadByUUID(uuid)
+
 	if err != nil {
 		error_message(writer, request, "Cannot read thread")
 	} else {
 		_, err := session(writer, request)
+
 		if err != nil {
 			generateHTML(writer, &thread, "layout", "public.navbar", "public.thread")
 		} else {
@@ -67,22 +75,29 @@ func postThread(writer http.ResponseWriter, request *http.Request) {
 		http.Redirect(writer, request, "/login", 302)
 	} else {
 		err = request.ParseForm()
+
 		if err != nil {
 			danger(err, "Cannot parse form")
 		}
+
 		user, err := sess.User()
+
 		if err != nil {
 			danger(err, "Cannot get user from session")
 		}
+
 		body := request.PostFormValue("body")
 		uuid := request.PostFormValue("uuid")
 		thread, err := data.ThreadByUUID(uuid)
+
 		if err != nil {
 			error_message(writer, request, "Cannot read thread")
 		}
+
 		if _, err := user.CreatePost(thread, body); err != nil {
 			danger(err, "Cannot create post")
 		}
+
 		url := fmt.Sprint("/thread/read?id=", uuid)
 		http.Redirect(writer, request, url, 302)
 	}
